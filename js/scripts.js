6a574d0b6736883c95992951f19cbb9a5d956d02
AREA <area.compiler@gmail.com>
2020-04-04 15:50:51 +0200

    lastColored_edu4

--- js/scripts.js
+++ js/scripts.js
@@ -100,11 +100,11 @@ $(document).ready(function() {
     var colorLi = $(".color-option ul li");
     colorLi 
     
-  .eq(0).css("background","#f4623a").end()
-  .eq(1).css("background","#f72d2d").end()
-  .eq(2).css("background","#17a2b8").end()
-  .eq(3).css("background","rgb(130, 77, 227)").end()
-  .eq(4).css("background","#ffa41b").end()
+  .eq(0).css("background","rgb(0, 189, 170)").end()
+  .eq(1).css("background","rgb(235, 69, 89)").end()
+  .eq(2).css("background","rgb(32, 64, 81)").end()
+  .eq(3).css("background","rgb(114, 27, 101)").end()
+  .eq(4).css("background","rgb(255, 164, 27)").end()
   // .eq(5).css("background","#000");
   
   $(".color-option ul li").click(function()
@@ -112,30 +112,40 @@ $(document).ready(function() {
     $(':root').css({
       // '--myprimeColor': $(this).css("background-color") ,
     });
-    if( $(this).css("background-color") == "rgb(244, 98, 58)" ){
+    if( $(this).css("background-color") == "rgb(0, 189, 170)" ){
       $(':root').css({
         '--myprimeColor': $(this).css("background-color") ,
-        '--myprimeColorblack': "#ee3e0d"
+        '--myprimeColorblack': "rgb(64, 0, 130)",
+        '--primeFontColor' : '#df154f'
+
       });
-    } else if( $(this).css("background-color") == "rgb(247, 45, 45)" ){
+    } else if( $(this).css("background-color") == "rgb(235, 69, 89)" ){
       $(':root').css({
         '--myprimeColor': $(this).css("background-color") ,
-        '--myprimeColorblack': "#a81a1a"
+        '--myprimeColorblack': "#f78259" ,
+        '--primeFontColor' : '#aeefec'
+
       });
-    } else if( $(this).css("background-color") == "rgb(23, 162, 184)" ){
+    } else if( $(this).css("background-color") == "rgb(32, 64, 81)" ){
       $(':root').css({
         '--myprimeColor': $(this).css("background-color") ,
-        '--myprimeColorblack': "#005081"
+        '--myprimeColorblack': "#3b6978",
+        '--primeFontColor' : "#f57b51"
+
       });
-    } else if( $(this).css("background-color") == "rgb(130, 77, 227)" ){
+    } else if( $(this).css("background-color") == "rgb(114, 27, 101)" ){
       $(':root').css({
         '--myprimeColor': $(this).css("background-color") ,
-        '--myprimeColorblack': "rgb(77, 43, 139)"
+        '--myprimeColorblack': "#b80d57",
+        '--primeFontColor' : "#c69eab"
+
       });
     } else if( $(this).css("background-color") == "rgb(255, 164, 27)" ){
       $(':root').css({
         '--myprimeColor': $(this).css("background-color") ,
-        '--myprimeColorblack': "#be7d1c"
+        '--myprimeColorblack': "#be7d1c",
+        '--primeFontColor' : "#000839"
+
       });
     }
     //  else if( $(this).css("background-color") == "rgb(0 , 0 , 0 )" ){
@@ -163,3 +173,36 @@ $(document).ready(function() {
 
 // // Set the color from array
 // document.documentElement.style.setProperty('--myprimeColor', getColor());
+
+$(document).ready(function() {
+  $(".reg").slideUp();
+
+
+});
+
+$(".Register").click(function(){
+  $(".reg").slideToggle();
+  $(".log").slideToggle();
+});
+
+var xx = 0 ;
+
+document.getElementById("show").onclick = function () {
+    if( xx==0 ){
+    document.getElementById("show").innerText = 'اخفاء كلمة السر';
+    document.getElementById("pwd").type = 'text';
+    document.getElementById("pwd2").type = 'text';
+    document.getElementById("pwd3").type = 'text';
+
+        xx++;
+    } else if( xx == 1){
+        document.getElementById("show").innerText = 'اظهار كلمة السر ';
+    document.getElementById("pwd").type = 'password';
+    document.getElementById("pwd2").type = 'password';
+    document.getElementById("pwd3").type = 'password';
+
+    
+
+        xx--;
+    }
+}
